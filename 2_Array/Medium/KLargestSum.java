88e0c5e072f3662c417043f8165aae90f268c820
VinayakUtekar <vinayakvutekar15@gmail.com>
2024-12-31 22:36:35 +0530

    Medium Array

--- 2_Array/Medium/KLargestSum.java
+++ 2_Array/Medium/KLargestSum.java
@@ -1,6 +1,19 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
-
 public class KLargestSum {
+    public static void Larg(int [] a, int k){
+        ArrayList<Integer> res = new ArrayList<>();
+        for (int i = 0; i < a.length; i++) {
+            int sum = 0;
+            for (int j = i; j < a.length; j++) {
+                sum = sum + a[j];
+                res.add(sum);
+            }
+        } 
+        Collections.sort(res,Collections.reverseOrder());
+        System.out.println(res.get(k-1));
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int nu = sc.nextInt();
@@ -8,7 +21,8 @@ public class KLargestSum {
         for (int i = 0; i < a.length; i++) {
             a[i] = sc.nextInt();
         }
-        Prod(a);
+        int k = sc.nextInt();
+        Larg(a,k);
         sc.close();
     }
 }
