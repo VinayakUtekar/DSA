918574e3e0870dc4514e81f3ff7993acb0ec1c85
VinayakUtekar <vinayakvutekar15@gmail.com>
2025-01-03 10:08:16 +0530

    Medium Array

---
+++ 2_Array/Hard/SurpasserCount.java
@@ -0,0 +1,13 @@
+public class SurpasserCount{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int [] a = new int[n];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = sc.nextInt();
+        }
+        int t = sc.nextInt();
+        Three(a,t);
+        sc.close();
+    }
+}
