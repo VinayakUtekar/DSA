b115491717d920e644231c51c0a7f6aa6330199a
VinayakUtekar <vinayakvutekar15@gmail.com>
2025-01-24 11:02:38 +0530

    Recursion Easy Problem

--- 6_Recursion/Easy/SumOfArray.java
+++ 6_Recursion/Easy/SumOfArray.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
+
 public class SumOfArray{
-    
+    public static int Sum(int [] a, int n){
+        if(n == 1){
+            return a[n-1];
+        }
+        else{
+            return (Sum(a, n-1) + a[n-1]);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int [] a = new int[n];
+        for(int i=0; i<n; i++){
+            a[i] = sc.nextInt();
+        }
+        int su = Sum(a, n);
+        System.out.println(su);
+        sc.close();
+    }
 }
